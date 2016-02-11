f6683066851a2d33b02be5c1ae59695526d1c948
Fogartybrian1975 <fogartybrian1975@eircom.net>
2016-02-11 20:26:09 +0000

    11/02/2016 20:30

--- src/main/java/com/brianf/games/configuration/HelloWorldConfiguration.java
+++ src/main/java/com/brianf/games/configuration/HelloWorldConfiguration.java
@@ -1,5 +1,5 @@
 package com.brianf.games.configuration;
-
+//making a simple addition Brian
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
